b32ccec662040bd5b195022303891abd0578bccc
João Maia <joaocarlosbgm@gmail.com>
2022-10-03 14:03:32 -0300

    refactor: linking db with express

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRouter from "./routes/users.routes";
+import { initDataBase } from "./database";
 
 const app = express();
 app.use(express.json());
@@ -10,4 +11,5 @@ app.use("/users", userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running in port ${port}`);
+  initDataBase();
 });
